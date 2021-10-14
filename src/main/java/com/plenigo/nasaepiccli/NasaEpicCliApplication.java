ee38d2e7025263cf9c0b3d215d356b877d9f8a82
Solaris86 <nikola.suscevic@hotmail.com>
2021-10-14 23:05:47 +0200

    Generated nasa-epic-cli project using spring initializr.

---
+++ src/main/java/com/plenigo/nasaepiccli/NasaEpicCliApplication.java
@@ -0,0 +1,13 @@
+package com.plenigo.nasaepiccli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaEpicCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaEpicCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plenigo/nasaepiccli/package-info.java
@@ -0,0 +1,4 @@
+/*
+ * This package contains all unit tests for nasa-epic-cli app
+ */
+package com.plenigo.nasaepiccli;
